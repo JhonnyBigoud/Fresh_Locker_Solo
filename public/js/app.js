552d7bd31496d3b099c4f8d79d391d13cabde37c
JhonnyBigoud <niset.alexis@hotmailfr>
2022-09-26 17:44:12 +0200

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,13 @@
+// Burger button/menu
+
+const hamburgerButton = document.querySelector(".nav-toggler");
+const navigation = document.querySelector(".nav-container");
+
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav() {
+    hamburgerButton.classList.toggle("active")
+    navigation.classList.toggle("active")
+}
+
+// Fin burger/button
